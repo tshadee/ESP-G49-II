8288c363331140d4841f201d528857fa8b1e1af4
Pannawich Techadee <pan.techadee@gmail.com>
2024-02-06 22:35:37 +0000

    tcrt class and PWMGen prototype

--- main.cpp
+++ main.cpp
@@ -3,33 +3,76 @@
 #include "mbed2/299/drivers/AnalogIn.h"
 #include "mbed2/299/drivers/Ticker.h"
 
+#define SENSOR_AMOUNT 5
+#define SENSOR_BUFFER 5
+#define SENSOR_POLL_FREQ 1000
+#define SUBSYS_POLL_FREQ 200
+#define SUBSYS_BUFFER 5
+#define GAIN_PROPORTIONAL 0.1
+#define GAIN_INTEGRAL 0.1
+#define GAIN_DERIVATIVE 0.1
+
+
 typedef enum {starting,straightline,curve,stop,turnaround} pstate;
 pstate ProgramState;
 
+//TCRT class. Creates an object for individual sensors on the sensor array with rolling average (size 5) built in. 
+//Provide the Pin (ADC) for the sensor and the voltage levels expected (if you want to scale to voltage instead of from 0.0 - 1.0)
+//Use getSensorNorm() to get value of voltage (rolled average). 
 class TCRT {
     private:
         AnalogIn sensorPin;
         float VDD,senseNorm;
-        float senseNormRolled[5];
+        float senseNormRolled[SENSOR_BUFFER];
         int rIndex;
+        static TCRT* sensors[SENSOR_BUFFER]; //operating with one ticker, so we must have a pointer to all the TCRT objects (to be used for function callback)
+        static int sensorCount; //to keep count of the sensors
     public:
-        TCRT(PinName Pin, float v): sensorPin(Pin), VDD(v){
+        TCRT(PinName Pin, float v): sensorPin(Pin), VDD(v)
+        {
             rIndex = 0;
             senseNorm = 0;
-            for(int i = 0;i<5;i++){senseNormRolled[i] = 0;};
-        }
-        float ampNorm(void){return (sensorPin.read());};
-        void rollingPollAverage(void)
+            for(int i = 0;i<SENSOR_BUFFER;i++){senseNormRolled[i] = 0;};
+            if(sensorCount < SENSOR_AMOUNT)
+            {
+                sensors[sensorCount++] = this; //appending individual sensor object pointers into the TCRT* pointer array to be used in static function pollSensors() later
+            };
+        };
+        void rollingPollAverage()
         {
-            senseNormRolled[rIndex%5] = ampNorm();
+            senseNormRolled[rIndex % SENSOR_BUFFER] = ampNorm();
             rIndex++;
             senseNorm = 0;
-            for(int i = 0;i<5;i++){senseNorm += senseNormRolled[i];};
-            senseNorm /= 5;
+            for(int i = 0;i < SENSOR_BUFFER;i++){senseNorm += senseNormRolled[i];};
+            senseNorm /= SENSOR_BUFFER;
         };
-        float getSensorNorm(void){return senseNorm;};
+        static void pollSensors(void) //runs through all the polling once called. This is for synchronous polling between sensors since static is shared between all objects derived from TCRT
+        {
+            for(int i = 0; i < sensorCount; i++) {
+                sensors[i]->rollingPollAverage();
+            };
+        };
+        float ampNorm(void){return (sensorPin.read());};
+        float getSensorNorm(bool Volt){return (Volt? senseNorm*VDD : senseNorm);}; 
 };
 
+TCRT* TCRT::sensors[SENSOR_AMOUNT]; //static member declaration (must be outside class)
+int TCRT::sensorCount = 0;  //static member declaration (must be outside class)
+
+//THIS CLASS INCLUDES PID AND PWM OUTPUT. Output rate will be tied to another Ticker. DO NOT MODIFY THIS YET.
+class PWMGen {
+    private:
+        float PWM_Internal_1, PWM_Internal_2;
+        DigitalOut PWM_OUT_CHANNEL_1, PWM_OUT_CHANNEL_2;
+        float IntegralBuffer[2], DerivativeBuffer[2];
+    public:
+        PWMGen(PinName P1, PinName P2): PWM_OUT_CHANNEL_1(P1),PWM_OUT_CHANNEL_2(P2){};
+
+
+};
+
+//LCD display buffer. Pass string pointers to display in lines 1-3 on the LCD screen. Keep the strings under 20 bytes if possible
+// call toScreen with the appropriate arguments to push anything to the LCD display - refresh rate sensitive (dont go above 15 Hz)
 void toScreen(char* line1, char*  line2, char* line3,C12832* lcd){
     static char lastLine1[21] = "";
     static char lastLine2[21] = "";
@@ -57,6 +100,10 @@ void toScreen(char* line1, char*  line2, char* line3,C12832* lcd){
 int main (void)
 {
     C12832 lcd(D11, D13, D12, D7, D10);
+    float sensorPollRate = 1.0/SENSOR_POLL_FREQ;
+    Ticker sensorPollTicker;
+    sensorPollTicker.attach(callback(&TCRT::pollSensors),sensorPollRate);
+
     while(1)
     {
 
